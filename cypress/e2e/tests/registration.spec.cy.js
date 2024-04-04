fecd754f2ecef42af1535d8015eca34f43bfc669
Bohdan Rybalka <021102rb@gmail.com>
2024-04-04 18:37:43 +0300

    Added the test. Added beforeach method to registration.spec. Added selectors for elements in registration PO. Added const object hobbies.

--- cypress/e2e/tests/registration.spec.cy.js
+++ cypress/e2e/tests/registration.spec.cy.js
@@ -1,8 +1,35 @@
-import {registrationPage} from "../../pages/RegistrationPage";
+import {hobbies, registrationPage} from "../../pages/RegistrationPage";
 
 describe('Registration e2e', () => {
-  it('Should be possible to navigate to the registration page', () => {
+  beforeEach(() => {
     registrationPage.navigateToPage();
+  });
+
+  it('Should be possible to navigate to the registration page', () => {
     registrationPage.elements.yourAverageFormTitle().should('be.visible');
+  });
+
+  it('Should be possible to see credentials, gender, hobbies and time sections on the registration page', () => {
+    registrationPage.elements.usernameLabel().should('be.visible');
+    registrationPage.elements.usernameInput().should('be.visible');
+    registrationPage.elements.passwordLabel().should('be.visible');
+    registrationPage.elements.passwordInput().should('be.visible');
+
+    registrationPage.elements.genderLabel().should('be.visible');
+    registrationPage.elements.maleRadioBtn().should('be.visible');
+    registrationPage.elements.femaleRadioBtn().should('be.visible');
+
+    registrationPage.elements.hobbyLabel().should('be.visible');
+    registrationPage.elements.hobbyItemName(hobbies.reading).should('be.visible');
+    registrationPage.elements.hobbyItemName(hobbies.sports).should('be.visible');
+    registrationPage.elements.hobbyItemName(hobbies.music).should('be.visible');
+    registrationPage.elements.hobbyItemCheckbox(hobbies.reading).should('be.visible');
+    registrationPage.elements.hobbyItemCheckbox(hobbies.sports).should('be.visible');
+    registrationPage.elements.hobbyItemCheckbox(hobbies.music).should('be.visible');
+
+    registrationPage.elements.timeLabel().should('be.visible');
+    registrationPage.elements.timeDropdown().should('be.visible');
+
+    registrationPage.elements.submitBtn().should('be.visible');
   });
 });
--- cypress/pages/RegistrationPage.js
+++ cypress/pages/RegistrationPage.js
@@ -1,10 +1,29 @@
 import {BasePage} from "./BasePage";
 
-export class RegistrationPage extends BasePage{
+export const hobbies = {
+    reading: 'Reading',
+    sports: 'Sports',
+    music: 'Music'
+}
+
+export class RegistrationPage extends BasePage {
     url = '/';
 
     elements = {
-        yourAverageFormTitle: () => cy.get('h1').contains('Your average form')
+        yourAverageFormTitle: () => cy.get('h1').contains('Your average form'),
+        usernameLabel: () => cy.get('label').contains('Username:'),
+        usernameInput: () => cy.get('#username'),
+        passwordLabel: () => cy.get('label').contains('Password:'),
+        passwordInput: () => cy.get('#password'),
+        genderLabel: () => cy.get('label').contains('Gender:'),
+        maleRadioBtn: () => cy.get('#genderMale'),
+        femaleRadioBtn: () => cy.get('#genderFemale'),
+        hobbyLabel: () => cy.get('label').contains('Hobbies:'),
+        hobbyItemName: (hobbyName) => cy.get('td').contains(hobbyName),
+        hobbyItemCheckbox: (hobbyName) => cy.get(`input[value=${hobbyName}]`),
+        timeLabel: () => cy.get('label').contains('Time:'),
+        timeDropdown: () => cy.get('#time'),
+        submitBtn: () => cy.get('button').type('submit')
     }
 }
 
